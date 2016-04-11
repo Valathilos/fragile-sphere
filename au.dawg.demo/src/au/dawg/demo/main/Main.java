0c8c1afda685a7e6181746402ac2359156281e49
Valathilos <valathilos@gmail.com>
2016-04-11 22:12:02 +1000

    Add a simple output change.

--- au.dawg.demo/src/au/dawg/demo/main/Main.java
+++ au.dawg.demo/src/au/dawg/demo/main/Main.java
@@ -3,6 +3,8 @@ package au.dawg.demo.main;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello world");
+		
+		System.out.println("Another message");
 	}
 	
 }
